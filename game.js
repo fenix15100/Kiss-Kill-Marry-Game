4be894f43fff693caf418f952e4f01b2598c7f4b
fenix15100 <francisco.camacho15@gmail.com>
2018-04-27 09:53:23 +0200

    revert cagada

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('bd.sqlite');
+require("./node_modules/jquery/jquery.js");
 let array=[];
 
 function Entidad(name,image){
@@ -10,12 +11,65 @@ function Entidad(name,image){
 
 db.serialize(function() {
 
-    db.each("SELECT name,image FROM Entity", function (err, row) {
-        e = new Entidad(row.name, row.image());
+    db.each("SELECT name,image FROM Entity", function (err,row) {
+        e = new Entidad(row.name, row.image);
         array.push(e);
     });
 });
 
 db.close();
 
-console.log(array);
+
+$("#jugada").click(function (e) {
+    e.preventDefault();
+    getEntity();
+
+});
+
+function getEntity() {
+    $("#gallery").empty();
+    $(".container-fluid ul").empty();
+
+    array=shuffle(array);
+    let jugada=array.slice(0,3);
+
+    console.log(jugada);
+
+    jugada.forEach(function (value) {
+
+        $('<img src="data:image/jpeg;base64,'+value.image+'"</img>').appendTo("#gallery");
+        $('<li id="nombre" class="text-muted">'+value.name+'</li></s><br>').appendTo(".container-fluid ul");
+
+
+
+    });
+
+}
+
+
+
+
+
+
+//Tipycall random int
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Funcion de un pavo porque la mierda de JS no tiene un puto shuffle
+function shuffle(arra1) {
+    let ctr = arra1.length, temp, index;
+
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
